6d27b62c6e4ea2769f7d849623ca6ffaa8a8f4d4
Liam Elliott <liamelliott1123@gmail.com>
2016-04-03 15:24:37 -0400

    Implement abstract method for creating a player and removing a player.

--- GOGameLogic/GoFish.cs
+++ GOGameLogic/GoFish.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GOContracts;
 
 namespace GOGameLogic
 {
-    class GoFish
+    public class GoFish : CardGame
     {
+        public GoFish()
+        {
+            Players = new List<IPlayer>();
+        }
+
+
+        public override IPlayer CreatePlayer(string name)
+        {
+            var player = new PlayerState(name);
+
+            Players.Add(player);
+
+            return player;
+        }
+
+        public override bool RemovePlayer(Guid player)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
