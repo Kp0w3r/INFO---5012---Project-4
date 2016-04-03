cbac72da1c3faf0101ed589a9d09b39dde045426
Liam Elliott <liamelliott1123@gmail.com>
2016-04-03 18:49:24 -0400

    Add Deck class implementation for contract interface.

--- GOContracts/DeckContract.cs
+++ GOContracts/DeckContract.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.ServiceModel;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace GOContracts
@@ -10,20 +11,59 @@ namespace GOContracts
     [ServiceContract]
     public interface ICallback
     {
-        [OperationContract(IsOneWay = true)]
-        void UpdateGui(GoCallback info);
     }
+
     [ServiceContract(CallbackContract = typeof(ICallback))]
     public interface IDeck
     {
         [OperationContract]
-        Card Draw();
+        ICard Draw();
         [OperationContract(IsOneWay = true)]
         void Shuffle();
         int NumCards { [OperationContract] get; }
-        [OperationContract]
-        Guid RegisterForCallbacks();
-        [OperationContract(IsOneWay = true)]
-        void UnregisterForCallbacks(Guid callbackId);
     }
+
+    public class Deck : IDeck
+    {
+        private Random _rand;
+
+        public Deck()
+        {
+            _rand = new Random();
+
+            Cards = new List<ICard>();
+        }
+
+        public int NumCards => Cards.Count;
+
+        public IList<ICard> Cards { get; }
+
+        public ICard Draw()
+        {
+            var lastCard = Cards.Last();
+
+            Cards.Remove(lastCard);
+
+            return lastCard;
+        }
+
+        public void Shuffle()
+        {
+            for (int i = 0; i < Cards.Count; i++)
+            {
+                // Add one to make upper bound inclusive
+                int limit = i + 1;
+                int swap = _rand.Next(0, limit);
+
+                if (swap != i)
+                {
+                    ICard card = Cards[swap];
+                    Cards[swap] = Cards[i];
+                    Cards[i] = card;
+                }
+            }
+        }
+
+    }
+
 }
