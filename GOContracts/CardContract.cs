69e3a27cf29a983871aa72a35cd443910ac3ecf4
Kp0w3r <kentpower@gmail.com>
2016-04-03 13:43:15 -0400

    Contracts
    Setup base contracts

---
+++ GOContracts/CardContract.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GOContracts
+{
+    //Implemented from Card example
+    public interface ICard
+    {
+        Card.SuitID Suit { get; }
+        Card.RankID Rank { get; }
+        String Name { get; }
+    }
+
+    [DataContract]
+    public class Card : ICard
+    {
+        public enum SuitID
+        {
+            Clubs, Diamonds, Hearts, Spades
+        };
+
+        public enum RankID
+        {
+            Ace, King, Queen, Jack, Ten, Nine, Eight, Seven, Six, Five, Four, Three, Two
+        };
+
+        [DataMember]
+        public SuitID Suit { get; private set; }
+
+        [DataMember]
+        public RankID Rank { get; private set; }
+
+        public String Name
+        {
+            get
+            {
+                return Rank.ToString() + " of " + Suit.ToString();
+            }
+        }
+
+        // Constructor
+        public Card(SuitID s, RankID r)
+        {
+            Suit = s;
+            Rank = r;
+        }
+    }
+}
---
+++ GOContracts/PlayerContract.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GOContracts
+{
+    interface IPlayer
+    {
+        string Name { get; }
+        Guid Id { get; }
+        bool hasCard { get; }
+    }
+
+    public class PlayerState : IPlayer
+    {
+        private List<ICard> _handCards;
+        private string _name;
+        private Guid _id;
+         
+        public string Name { get; }
+        public Guid Id { get; }
+        public bool hasCard { get; }
+
+        PlayerState(string Name)
+        {
+            
+        }
+    }
+}
