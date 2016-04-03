0f34a872a2ec6250ebea45b0a169d54152cf57b7
Liam Elliott <liamelliott1123@gmail.com>
2016-04-03 13:47:39 -0400

    Add abstract class to represent a card game object.

---
+++ GOGameLogic/CardGame.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GOGameLogic
+{
+    public abstract class CardGame
+    {
+
+        protected CardGame()
+        {
+            GameId = Guid.NewGuid();
+        }
+
+        /// <summary>
+        /// Gets a <see cref="Guid"/> uniquely identifying the game instance.
+        /// </summary>
+        public Guid GameId { get; }
+
+        /// <summary>
+        /// Gets an <see cref="IEnumerable{IPlayer}"/> containing player information for all players in the game.
+        /// </summary>
+        public IEnumerable<IPlayer> Players { get; protected set; }
+    }
+}
