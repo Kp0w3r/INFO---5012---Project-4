37bd414d7e90b45446f8771aaa185e72c3c85638
Kp0w3r <kentpower@gmail.com>
2016-04-03 13:12:25 -0400

    Remove Class 1
    removed class 1

--- GOServiceHost/Program.cs
+++ GOServiceHost/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
+using GOGameLogic;
+using GOContracts;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +13,26 @@ namespace GOServiceHost
     {
         static void Main(string[] args)
         {
+            ServiceHost servHost = null;
+
+            //Code Taken From Cards example
+            try
+            {
+                servHost = new ServiceHost(typeof(CardsLibrary.Shoe));
+
+                servHost.Open();
+                Console.WriteLine("Shoe service started. Press a key to quit.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+                if (servHost != null)
+                    servHost.Close();
+            }
         }
     }
 }
