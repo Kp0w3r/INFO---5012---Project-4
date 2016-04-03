21f6ad31d0c61054e7d67506fb9a58169f865246
Liam Elliott <liamelliott1123@gmail.com>
2016-04-03 15:23:31 -0400

    Add abstract methods to add and remove a player from the game and change Players to IList<IPlayer>.

--- GOGameLogic/CardGame.cs
+++ GOGameLogic/CardGame.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GOContracts;
 
 namespace GOGameLogic
 {
@@ -22,6 +23,21 @@ namespace GOGameLogic
         /// <summary>
         /// Gets an <see cref="IEnumerable{IPlayer}"/> containing player information for all players in the game.
         /// </summary>
-        public IEnumerable<IPlayer> Players { get; protected set; }
+        public IList<IPlayer> Players { get; protected set; }
+
+        /// <summary>
+        /// Creates a new player and returns its instance.
+        /// </summary>
+        /// <param name="name">The display name of the player.</param>
+        /// <returns>A reference to an <see cref="IPlayer"/> instance of the player created.</returns>
+        public abstract IPlayer CreatePlayer(string name);
+
+        /// <summary>
+        /// Removes a specified player from the game.
+        /// </summary>
+        /// <param name="player">The guid identity of the player.</param>
+        /// <returns>A bool value indicating whether the player was successfully removed.</returns>
+        public abstract bool RemovePlayer(Guid player);
+
     }
 }
