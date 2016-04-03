1aebd6f150a1b45bcbe2de36cead1423df83d080
Kp0w3r <kentpower@gmail.com>
2016-04-03 13:06:24 -0400

    Setup Project
    Add all projects needed

---
+++ GOServiceHost/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GOServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
