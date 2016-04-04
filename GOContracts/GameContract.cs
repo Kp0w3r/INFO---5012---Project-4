1bcbe06bd15634185f4a6c680767f57eccb58bdd
Kp0w3r <kentpower@gmail.com>
2016-04-04 11:17:16 -0400

    Contracts
    added game contract

---
+++ GOContracts/GameContract.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GOContracts
+{
+    [ServiceContract(CallbackContract = typeof(ICallback))]
+    interface IGame
+    {
+        Guid GameId { [OperationContract] get; }
+        IList<IPlayer> Players { [OperationContract] get; }
+        [OperationContract]
+        IPlayer CreatePlayer(string name);
+        [OperationContract]
+        bool RemovePlayer(Guid player);
+    }
+}
