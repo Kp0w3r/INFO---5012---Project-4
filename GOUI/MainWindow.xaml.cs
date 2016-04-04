632e90f7a995e1370ad0b6836f451e92c807fe43
Kp0w3r <kentpower@gmail.com>
2016-04-04 11:22:38 -0400

    GOUI
    changed Ideck to IGame
    defaulted player name to tonygeorge

--- GOUI/MainWindow.xaml.cs
+++ GOUI/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using GOContracts;
 
 namespace GOUI
 {
@@ -20,14 +22,44 @@ namespace GOUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private IGame _game = null;
+        public IPlayer playerData = null;
         public MainWindow()
         {
             InitializeComponent();
+
+            this.Loaded += OnLoaded;
+            this.Closed += OnClosed;
         }
+
+        private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            DuplexChannelFactory<IGame> deckFactory = new DuplexChannelFactory<IGame>(this, "GameService");
+            _game = deckFactory.CreateChannel();
+
+            playerData = _game.CreatePlayer("TonyGeorge");
+        }
+
+        private void OnClosed(object sender, EventArgs eventArgs)
+        {
+            if (_game != null && playerData != null)
+            {
+                try
+                {
+                    _game.RemovePlayer(playerData.Id);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+
+        }
+
+
 
         private void Selector_OnSelected(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
         }
     }
 }
