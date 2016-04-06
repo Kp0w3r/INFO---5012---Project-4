01521bec9d7c17b96355bb6933ac5fe4369f506b
Liam Elliott <liamelliott1123@gmail.com>
2016-04-06 09:47:08 -0400

    Remove operationcontracts and HasCard method.

--- GOContracts/PlayerContract.cs
+++ GOContracts/PlayerContract.cs
@@ -9,31 +9,22 @@ using System.Threading.Tasks;
 
 namespace GOContracts
 {
-    [ServiceContract(CallbackContract = typeof(ICallback))]
     public interface IPlayer
     {
         /// <summary>
         /// Gets the player's display name.
         /// </summary>
-        string Name { [OperationContract] get; }
+        string Name { get; }
 
         /// <summary>
         /// Gets the Guid identity of the player.
         /// </summary>
-        Guid Id { [OperationContract] get; }
+        Guid Id { get; }
 
         /// <summary>
         /// Gets an <see cref="IList{ICard}"/> of the cards in the player's hand.
         /// </summary>
-        IList<ICard> Hand { [OperationContract] get; }
-
-        /// <summary>
-        /// Determines whether a player has a specified card.
-        /// </summary>
-        /// <param name="card">The card to look for.</param>
-        /// <returns>True if the card exists and false if it does not.</returns>
-        [OperationContract]
-        bool HasCard(ICard card);
+        IList<ICard> Hand { get; }
     }
 
     [DataContract]
@@ -48,10 +39,6 @@ namespace GOContracts
         [DataMember]
         public IList<ICard> Hand { get; set; }
 
-        public bool HasCard(ICard card)
-        {
-            throw new NotImplementedException();
-        }
         public Player(string name, Guid id)
         {
             this.Name = name;
